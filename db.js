b2395f0f65def8343212d1f87d5cf2ce7d0332da
Elizabeth Ha <elizabeth.ha@nyu.edu>
2018-12-06 21:53:49 -0500

    db changes

--- db.js
+++ db.js
@@ -21,4 +21,23 @@ mongoose.model('Post', postSchema);
 mongoose.model('User', userSchema);
 mongoose.model('Review', reviewSchema);
 
-mongoose.connect('mongodb://localhost/snippet-users');
+// is the environment variable, NODE_ENV, set to PRODUCTION?
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from a file
+ // use blocking file io to do this...
+ const fs = require('fs');
+ const path = require('path');
+ const fn = path.join(__dirname, 'config.json');
+ const data = fs.readFileSync(fn);
+
+ // our configuration file will be in json, so parse it and set the
+ // conenction string appropriately!
+ const conf = JSON.parse(data);
+ dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://localhost/snippet-users';
+}
+// mongoose.connect('mongodb://localhost/snippet-users');
+mongoose.connect(dbconf);
